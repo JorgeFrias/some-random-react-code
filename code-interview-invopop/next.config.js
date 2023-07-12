5bfdaa722cafd15672e7bb511357583df71a5a39
Jorge Frias <jorge@reliby.com>
2023-07-12 15:01:49 +0200

    [Setup] Exporting SCSS variables as well as setting up the SCSS compiler.

--- code-interview-invopop/next.config.js
+++ code-interview-invopop/next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const path = require('path')
+
+const nextConfig = {
+    scssOptions: {
+        includePaths: [path.join(__dirname, 'styles'), __dirname],
+      },
+}
 
 module.exports = nextConfig
