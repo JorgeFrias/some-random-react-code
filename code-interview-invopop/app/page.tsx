dfa5c79659940ac697e178eed92c1030849ca1af
Jorge Frias <jorge@reliby.com>
2023-07-13 03:32:42 +0200

    Added the headers.

--- code-interview-invopop/app/page.tsx
+++ code-interview-invopop/app/page.tsx
@@ -2,13 +2,23 @@
 
 import React, { useState, useEffect } from "react";
 import { MainContainer } from "@/components/layout/MainContainerComponent";
+import { HeadingComponent } from "@/components/textElements/HeadingComponent";
 
 export default function Home() {
   return (
     <main>
       <MainContainer
-        primaryView={<div>Primary View</div>}
-        sideView={<div>Side View</div>}
+        primaryView={
+          <div>
+            <HeadingComponent>Shirt</HeadingComponent>
+            
+          </div>
+        }
+        sideView={
+          <div>
+            <HeadingComponent>Shirt</HeadingComponent>
+          </div>
+        }
         // hasCloseButton={true}
         // onClose={() => {}}
       />
